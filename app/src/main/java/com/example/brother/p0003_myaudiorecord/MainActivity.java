83ef38c6d927ce4a089f8c8981bfb9ec2c536772
metisdesign <designmetis@gmail.com>
2016-05-07 22:07:22 +0300

    first commit

---
+++ app/src/main/java/com/example/brother/p0003_myaudiorecord/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.brother.p0003_myaudiorecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
